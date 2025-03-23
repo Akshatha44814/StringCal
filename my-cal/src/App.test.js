09c5eeec9e53ffa02c945fff19fedbe2d8176012
Akshatha M <akshatham9@gmail.com>
2025-03-23 09:43:22 +0530

    Add the test cases

--- my-cal/src/App.test.js
+++ my-cal/src/App.test.js
@@ -1,20 +1,73 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from './App';
-import Calculator from './Calculator/Calculator';
+import App from "./App";
+import Calculator from "./Calculator/Calculator";
+import "./index.css";
+import "./Calculator/Calculator.css";
 
-test('renders Main App Component', () => {
+afterEach(cleanup);
+
+test("renders Main App Component", () => {
   render(<App />);
 });
-test('renders Main Calculate Component', () => {
-  render(<Calculator />);
-  const linkHeader = screen.getByText("String Calculator")
-  expect(linkHeader).toBeInTheDocument();
+describe("renders Calculate Component", () => {
+  it("elements of the page", () => {
+    render(<Calculator />);
+    const linkHeader = screen.getByText("String Calculator");
+    expect(linkHeader).toBeInTheDocument();
 
-  const linkLabel = screen.getByText('Enter numbers')
-  expect(linkLabel).toBeInTheDocument();
+    const linkLabel = screen.getByText("Enter numbers");
+    expect(linkLabel).toBeInTheDocument();
+    const inputStringField = screen.getByPlaceholderText(
+      "E.G.,//;\\n1;2;3 or 1,2,3"
+    );
+    expect(inputStringField).toBeInTheDocument();
+    const value = "1,2,3";
+    fireEvent.change(inputStringField, {
+      target: {
+        value,
+      },
+    });
+    expect(inputStringField).toHaveValue("1,2,3");
 
-  const buttonEl = screen.getByText("Calculate");
-  userEvent.click(buttonEl);
-  expect(buttonEl).toHaveTextContent("Calculate");
+    const buttonEl = screen.getByText("Calculate");
+    userEvent.click(buttonEl);
+    expect(buttonEl).toHaveTextContent("Calculate");
+
+    const outputStringField = screen.getByPlaceholderText("Result Output");
+    expect(outputStringField).toBeInTheDocument();
+    expect(outputStringField).toBeDisabled();
+    expect(outputStringField).toHaveValue("6");
+  });
+
+  it("display the format label", () => {
+    const Format = () => (
+      <p>
+        {"(Format:"}
+        <span>{"//[delimiter]\n[numbers]"}</span>
+        {")"}
+      </p>
+    );
+    const { asFragment } = render(<Format />);
+    expect(asFragment()).toHaveTextContent("(Format://[delimiter] [numbers])");
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render the value of negative number and error message", () => {
+    render(<Calculator negativeNumbers="-1,-2" error={true} />);
+    const inputStringField = screen.getByPlaceholderText(
+      "E.G.,//;\\n1;2;3 or 1,2,3"
+    );
+    expect(inputStringField).toBeInTheDocument();
+    const NegativeError = () => (
+      <span className="content colorContent">
+        {"Negative numbers not allowed :-1,-2"}
+      </span>
+    );
+    const { asFragment } = render(<NegativeError />);
+
+    expect(asFragment()).toHaveTextContent(
+      "Negative numbers not allowed :-1,-2"
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
--- my-cal/src/Calculator/Calculator.js
+++ my-cal/src/Calculator/Calculator.js
@@ -1,18 +1,86 @@
-import "./Calculator.css"
+import { useState } from "react";
+import "./Calculator.css";
 
-const Calculator = () =>{
-    return <div className="conatiner">
-        <h1>String Calculator</h1>
-        <div className="content">
-            Enter numbers
+const Calculator = () => {
+  const [result, setResult] = useState("");
+  const [calculate, setCalculate] = useState("");
+  const [error, setError] = useState(false);
+  const [negativeNumbers, setNegativeNumbers] = useState([]);
+
+  const inputMethod = (e) => {
+    const inputValue = e.target.value.split("");
+    if (inputValue.includes("-")) {
+      setError(true);
+      const res = e.target.value.match(/-?\d+/g).filter((a, b) => {
+        if (a.includes("-")) {
+          return true;
+        }
+      });
+      setNegativeNumbers(res.join(","));
+    } else {
+      setError(false);
+      const findNumber =
+        inputValue.length > 0 &&
+        inputValue.filter((a, b) => {
+          if (!isNaN(parseInt(a))) {
+            return true;
+          }
+        });
+      const resultOutput =
+        findNumber.length > 0
+          ? findNumber?.reduce((c, d) => {
+              return parseInt(c) + parseInt(d);
+            })
+          : 0;
+      setCalculate(resultOutput);
+    }
+  };
+  const calculateSum = (e) => {
+    setResult(calculate);
+  };
+  return (
+    <div className="conatiner">
+      <h1>String Calculator</h1>
+      <p className="content">Enter numbers</p>
+      <p className="content">
+        {"(Format:"}
+        <span className="colorContent" data-testid="redColor">{"//[delimiter]\n[numbers]"}</span>
+        {")"}
+      </p>
+      <div>
+        <input
+          type="text"
+          className="input"
+          placeholder="E.G.,//;\n1;2;3 or 1,2,3"
+          onChange={(e) => inputMethod(e)}
+        />
+      </div>
+
+      <button
+        className="calculate"
+        onClick={(e) => {
+          calculateSum();
+        }}
+        disabled={error}
+      >
+        Calculate
+      </button>
+      {error ? (
+        <span className="content colorContent">
+          Negative numbers not allowed :{negativeNumbers}
+        </span>
+      ) : (
+        <div className="resultInput">
+          <input
+            type="text"
+            className="input"
+            value={`${result}`}
+            placeholder="Result Output"
+            disabled
+          />
         </div>
-        <div className="content">
-            (Format:<span className="colorContent">//[delimiter]\n[numbers]</span>)
-        </div>
-        <div>
-            <input type="text" className="input" placeholder="E.G.,//;\n1;2;3 or 1,2,3"/>
-        </div>
-        <button className="calculate">Calculate</button>
+      )}
     </div>
-}
-export default Calculator
+  );
+};
+export default Calculator;
