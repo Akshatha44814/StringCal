e0742e602e4184c5909c44371d50a55511cf50a0
Akshatha M <akshatham9@gmail.com>
2025-03-24 18:31:17 +0530

    render input field with on change value

--- src/Calculator/Calculator.test.js
+++ src/Calculator/Calculator.test.js
@@ -1,9 +1,23 @@
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import CalculatorPage from "../CalculatorPage/CalculatorPage";
 
 afterEach(cleanup);
 describe("renders Calculate Component", () => {
   it("Render Calculate Body Component", () => {
     render(<CalculatorPage />);
+  });
+  it("Input string Onclick calculate show output", () => {
+    render(<CalculatorPage />);
+    const inputStringField = screen.getByPlaceholderText(
+      "E.G.,//;\\n1;2;3 or 1,2,3"
+    );
+    expect(inputStringField).toBeInTheDocument();
+    const value = "1,2,3";
+    fireEvent.change(inputStringField, {
+      target: {
+        value,
+      },
+    });
+    expect(inputStringField).toHaveValue("1,2,3");
   });
 });
