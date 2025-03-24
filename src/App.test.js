917d4524e09e78414c816e16cb079338f2bc92f2
Akshatha M <akshatham9@gmail.com>
2025-03-24 18:26:27 +0530

    Render Enter numbers label

--- src/App.test.js
+++ src/App.test.js
@@ -13,11 +13,20 @@ describe("renders Calculate Component", () => {
     render(<Calculator />);
   });
   it("render String Calculator header", () => {
-      const { asFragment } = render(<Calculator />);
-      const linkHeader = screen.getByText("String Calculator");
-      expect(linkHeader).toBeInTheDocument();
-      expect(asFragment()).toHaveTextContent("String Calculator");
-      expect(asFragment()).toMatchSnapshot();
-      expect(asFragment()).toBeTruthy();
-    });
+    const { asFragment } = render(<Calculator />);
+    const linkHeader = screen.getByText("String Calculator");
+    expect(linkHeader).toBeInTheDocument();
+    expect(asFragment()).toHaveTextContent("String Calculator");
+    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toBeTruthy();
+  });
+
+  it("render Enter numbers label", () => {
+    const { asFragment } = render(<Calculator />);
+    const linkLabel = screen.getByText("Enter numbers");
+    expect(linkLabel).toBeInTheDocument();
+    expect(asFragment()).toHaveTextContent("Enter numbers");
+    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toBeTruthy();
+  });
 });
