0b981ea3fcf28d918c891624da0d9e8259d3efe3
Akshatha M <akshatham9@gmail.com>
2025-03-24 21:22:21 +0530

    deployed website

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,15 +5,15 @@ const Calculator = () => {
   return (
     <div className="conatiner">
       <h1>String Calculator</h1>
-      <p className="content">Enter numbers</p>
-      <p className="content">
-        {"(Format:"}
-        <span className="colorContent" data-testid="redColor">
-          {"//[delimiter]\n[numbers]"}
-        </span>
-        {")"}
-      </p>
-      <CalculatorPage/>
+        <p className="content">Enter numbers</p>
+        <p className="content">
+          {"(Format:"}
+          <span className="colorContent" data-testid="redColor">
+            {"//[delimiter]\n[numbers]"}
+          </span>
+          {")"}
+        </p>
+        <CalculatorPage />
     </div>
   );
 };
